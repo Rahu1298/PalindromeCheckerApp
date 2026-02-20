2830b52dd84941fc5f4c557a9eeb93bf59343fb9
Rahu1298 <naikrishabh35@gmail.com>
2026-02-20 15:58:36 +0530

    UC6:Stack-Based Palindrome Checker

--- main.java
+++ main.java
@@ -1,10 +1,12 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class main {
     public static void main ( String [] args ) {
-        System.out.println("Welcome to the Palindrome Checker Management System!");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully");
+        // System.out.println("Welcome to the Palindrome Checker Management System!");
+        // System.out.println("Version : 1.0");
+        // System.out.println("System initialized successfully");
 
         // String Literal stored in String constant pool
         String word = "madam";
@@ -23,20 +25,49 @@ public class main {
         //     System.out.println(word + " is NOT a Palindrome");
         // }
 
-        Stack<Character> stack = new Stack<>();
+        // Stack<Character> stack = new Stack<>();
 
-        // Push characters into stack
+        // // Push characters into stack
+        // for (int i = 0; i < word.length(); i++) {
+        //     stack.push(word.charAt(i));
+        // }
+
+        // boolean isPalindrome = true;
+
+        // // Pop and compare
+        // for (int i = 0; i < word.length(); i++) {
+        //     char poppedChar = stack.pop();
+
+        //     if (word.charAt(i) != poppedChar) {
+        //         isPalindrome = false;
+        //         break;
+        //     }
+        // }
+
+        // if (isPalindrome) {
+        //     System.out.println(word + " is a Palindrome");
+        // } else {
+        //     System.out.println(word + " is NOT a Palindrome");
+            
+         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        // Step 1: Push into stack & Enqueue into queue
         for (int i = 0; i < word.length(); i++) {
-            stack.push(word.charAt(i));
+            char ch = word.charAt(i);
+            stack.push(ch);      // LIFO
+            queue.add(ch);       // FIFO
         }
 
         boolean isPalindrome = true;
 
-        // Pop and compare
-        for (int i = 0; i < word.length(); i++) {
-            char poppedChar = stack.pop();
+        // Step 2: Compare pop (stack) with dequeue (queue)
+        while (!stack.isEmpty()) {
 
-            if (word.charAt(i) != poppedChar) {
+            char fromStack = stack.pop();      // Reverse order
+            char fromQueue = queue.remove();   // Original order
+
+            if (fromStack != fromQueue) {
                 isPalindrome = false;
                 break;
             }
@@ -47,5 +78,7 @@ public class main {
         } else {
             System.out.println(word + " is NOT a Palindrome");
         }
+
+        }
     }
-}
+
