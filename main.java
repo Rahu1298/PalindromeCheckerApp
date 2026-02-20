15ee6b664ab199635b37ec36664d70843afc7300
Rahu1298 <naikrishabh35@gmail.com>
2026-02-20 15:14:52 +0530

    UC2-Print a Hardcoded Palindrome Result

--- main.java
+++ main.java
@@ -1,8 +1,24 @@
-import java.lang.System;
 public class main {
     public static void main ( String [] args ) {
         System.out.println("Welcome to the Palindrome Checker Management System!");
         System.out.println("Version : 1.0");
         System.out.println("System initialized successfully");
+
+        // String Literal stored in String constant pool
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Conditional Statement (if-else)
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
     }
 }
